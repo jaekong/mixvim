1740a37651bbc1d1f14c9537f42bc6073e8cb65a
Jae Kong <hello@jtoda.one>
2024-10-01 22:03:29 +0900

    feat: update xcodebuild.nvim

--- plugins/extraPlugins.nix
+++ plugins/extraPlugins.nix
@@ -12,7 +12,7 @@ let
     };
   };
 
-  xcode-nvim = lib.mkIf xcodeEnable (fromGitHub "v3.5.1" "wojciech-kulik" "xcodebuild.nvim" "sha256-AUYMOasLldv0WAlNe8qOWpgDNx31v7hxFbhncuTg7Hs=");
+  xcode-nvim = lib.mkIf xcodeEnable (fromGitHub "v3.6.0" "wojciech-kulik" "xcodebuild.nvim" "sha256-LPf9MSdl7+4WVMH3t6slokO0xZ2Q8OBoJIlvvjj2QOU=");
 in
 {
   extraPlugins = lib.mkMerge (with pkgs.vimPlugins; [
